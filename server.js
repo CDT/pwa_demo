0ae056589b7d2f5dfc18af8ec8ef20dbc8f501bb
CDT <779888925@qq.com>
2023-12-21 21:35:52 +0800

    .

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const https = require('https');
-const fs = require('fs');
+const express = require('express')
+const https = require('https')
+const fs = require('fs')
 
-const app = express();
+const app = express()
 
 // Serve static files from the "public" directory
-app.use(express.static('.'));
+app.use(express.static('.'))
 
 // Create an HTTPS server with self-signed certificates
 const options = {
   key: fs.readFileSync('./ssl/server.key'),
   cert: fs.readFileSync('./ssl/server.cert')
-};
+}
 
-const server = https.createServer(options, app);
+const server = https.createServer(options, app)
 
 server.listen(3000, () => {
-  console.log('Server listening on port 3000 (HTTPS)');
-});
+  console.log('Server listening on port 3000 (HTTPS)')
+})
